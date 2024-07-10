0e3ba36774d1ce3900dd2a0279117eb1f72d6f4a
sunn02 <sollopez0002@hotmail.com>
2024-07-10 11:40:12 -0400

    6

--- ch6.py
+++ ch6.py
@@ -27,15 +27,13 @@ if __name__ == '__main__':
     # 50
     #  /     \
     # 30     70
-    #  / \ / \
-    # 20 40 60 80
+    #  / \ 
+    # 20 40 
  
     r = Nodo(50)
     r = insert(r, 30)
     r = insert(r, 20)
     r = insert(r, 40)
     r = insert(r, 70)
-    r = insert(r, 60)
-    r = insert(r, 80)
  
     inorder(r)
